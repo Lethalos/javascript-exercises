a3dc76c05faf2f35d5072dcdb3059e06964a76a8
KaanOzdogan <necdetkaanoz@hotmail.com>
2022-10-01 22:11:43 +0300

    Add exercise 3 solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  let reverseStr = [];
+  str.split("").forEach((char) => {
+    reverseStr.unshift(char);
+  });
 
+  return reverseStr.join("");
 };
 
 // Do not edit below this line
