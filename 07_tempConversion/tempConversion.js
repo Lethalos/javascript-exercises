eac771cf9e2cc53288ae0586f987ae3f2a4ee108
KaanOzdogan <necdetkaanoz@hotmail.com>
2022-10-02 02:23:32 +0300

    Add exercise 7 solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
-const ftoc = function() {
-
+const ftoc = (degree) => {
+  return Number(((degree - 32) / 1.8).toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = (degree) => {
+  return Number((degree * 1.8 + 32).toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
