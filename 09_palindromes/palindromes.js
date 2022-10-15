812b14534d8f6e436f12fc5312e1ca6089c7cc79
KaanOzdogan <necdetkaanoz@hotmail.com>
2022-10-15 18:08:10 +0300

    Add exercise 9 solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const regex = /[^a-z0-9]/gi;
+  const replaced = str.replace(regex, "").toLowerCase();
 
+  for (let i = 0; i < replaced.length; i++) {
+    if (replaced[i] != replaced[replaced.length - (i + 1)]) {
+      return false;
+    }
+  }
+
+  return true;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
